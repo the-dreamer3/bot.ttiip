c54fb032e2db5d58eda22014f5d4c819a3900a32
the-dreamer3 <153685695+the-dreamer3@users.noreply.github.com>
2024-12-25 10:17:45 +0200

    121212

--- messages.py
+++ messages.py
@@ -2,4 +2,4 @@ def hello(name, kurs):
     return f"Приветствуем, *{name} из {kurs} группы*!\n\nВ боте информирования\n#-#-#   _Тираспольского техникума Информатики и права_    #-#-#"
 
 def Contacts():
- return f"*Основные контакты:*\n*[Приемная]*-- `0(533)4-44-13`\n*[учебная часть]*  -- `0(533)4-23-95`\n[заказать справку онлайн](https://docs.google.com/forms/d/e/1FAIpQLSecROwSrMMLz2tJ7fz03lywdFpLRDaf6GhjpVJ9sWGOq6Ky-Q/viewform)"
+    return f"*Основные контакты:*\n*[Приемная]* -- `0(533)4-44-13`\n*[Учебная часть]* -- `0(533)4-23-95`\n[Заказать справку онлайн](https://docs.google.com/forms/d/e/1FAIpQLSecROwSrMMLz2tJ7fz03lywdFpLRDaf6GhjpVJ9sWGOq6Ky-Q/viewform)"
